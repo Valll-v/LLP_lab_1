44ce1895b5c3c95b4521db92bb796de86621c4e6
Valll-v <stepanenko.valera2013@gmail.com>
2023-12-05 13:54:49 +0300

    ifdef for linux/windows

--- src/db_communication.c
+++ src/db_communication.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+#ifdef _WIN32
 
 void printHello() {
-    printf("Hello\n");
+    printf("Hello windows!\n");
 }
+
+#elif __linux__
+
+void printHello() {
+    printf("Hello linux!\n");
+}
+
+#else
+
+void printHello() {
+    printf("Hello... who are you?\n");
+}
+
+#endif
